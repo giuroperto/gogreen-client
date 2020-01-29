ac0c4604262d8ae9b4dccbf56d93b0540d123f40
henricasi <henriquecmandruzato@gmail.com>
2020-01-29 10:30:28 -0300

    Components setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import AboutUs from './components/AboutUs'
+import AllRecipes from './components/AllRecipes'
+import AddRecipe from './components/AddRecipe'
+import Profile from './components/Profile'
+import EditProfile from './components/EditProfile'
+import RecipeDetails from './components/RecipeDetails'
+import EditRecipe from './components/EditRecipe'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/signup' component={Signup}/>
+        <Route exact path='/aboutus' component={AboutUs}/>
+        <Route exact path='/main' component={AllRecipes}/>
+        <Route exact path='/addrecipe' component={AddRecipe}/>
+        <Route exact path='/user/:username' component={Profile}/> 
+        <Route exact path='/user/:username/edit' component={EditProfile}/> 
+        <Route exact path='/recipe/:id' component={RecipeDetails}/>
+        <Route exact path='/recipe/:id/edit' component={EditRecipe}/>
+      </Switch>
     </div>
   );
 }
 
 export default App;
-
-// push
---
+++ src/components/RecipeDetails.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return()
+  }
+}
+
+export default Navbar;
