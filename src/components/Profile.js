29f556c1bbd4c487812efa7e78838e1deac8d5ae
Giu Roperto <giu.roperto@gmail.com>
2020-02-01 19:05:41 -0300

    started integrating back and front in profile + navbar logged user + update loggedin user app.js + history

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -18,11 +18,6 @@ class Profile extends Component {
     // this.handleChange = this.handleChange.bind(this);
   }
 
-  // componentDidMount() {
-  //   this.setState({
-  //     loggedInUser: this.props.loggedInUser,
-  //   })
-
   componentDidMount() {
     const { username } = this.props.match.params;
     this.apiEndpoints.getOneUser(username)
@@ -34,15 +29,8 @@ class Profile extends Component {
       .catch(err => console.log(err));
   }
 
-  // favoritesToggle() {
-  //   this.setState({
-  //     showFavourites: !this.state.showFavourites,
-  //   })
+  // showFavourites() {
   // }
-
-  showFavourites() {
-    // const { username } = this.props.match.params;
-  }
 
   render() {
     return(
@@ -83,3 +71,59 @@ class Profile extends Component {
 }
 
 export default Profile;
+
+// this.state = {
+//   uniqueUser: {},
+//   recipesToShow: []
+// }
+// this.favoritesToggle = this.favoritesToggle.bind(this);
+// this.myRecipesToggle = this.myRecipesToggle.bind(this);
+// }
+
+// componentDidMount() {
+// const {username} = this.props.match.params;
+// axios.get(`/user/${username}`)
+// .then(response => this.setState({
+//   uniqueUser: response.data,
+//   recipesToShow: response.data.recipes
+// }))
+// .catch(error => console.log(error));
+// }
+
+// favoritesToggle() {
+// this.setState({
+//   recipesToShow: this.uniqueUser.favorites
+// })
+// }
+
+// myRecipesToggle() {
+// this.setState({
+//   recipesToShow: this.uniqueUser.recipes
+// })
+
+// const {uniqueUser} = this.state;
+//     return(
+//       <div className="profile-page">
+//         <div className="user-info d-flex">
+//           <div className="profile-picture">
+//             <img src={uniqueUser.picture} alt="Profile picture"/>
+//           </div>
+//           <div className="infos">
+//             <h2>{uniqueUser.username}</h2>
+//             <p class="name">{uniqueUser.firstName} {uniqueUser.lastName}</p>
+//             <p>User since {uniqueUser.createdAt}</p>
+//             <p>Has contributed {uniqueUser.recipes.length} recipes</p>
+//           </div>
+//         </div>
+//         <div className="user-recipes">
+//           <div className="links">
+//             <a href='#' onClick={this.myRecipesToggle}>Recipes</a>
+//             <a href='#' onClick={this.favoritesToggle}>Favorites</a>
+//           </div>
+//           <div className="recipes-cards-container">
+//             {this.state.recipesToShow === {} && <p>No recipes to show!</p> }
+//             {this.state.recipesToShow.map(recipe => <RecipeCard {...recipe}/>)}
+//           </div>
+//         </div>
+//       </div>
+//     )
