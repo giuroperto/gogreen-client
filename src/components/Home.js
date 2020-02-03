3a34dd3b813bb05efaae589b897376f0a00c5357
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-02-02 21:19:32 -0300

    homepage updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,14 +10,55 @@ class Home extends Component {
   render() {
     return (
       <div className="home-container">
-        <section className="section1-container">
-          Hello
+        <section className="d-flex justify-content-center align-items-center section1-container">
+          <div className="section1-div">
+            <h2>GoGreen</h2>
+            <h4>Find and share V recipes</h4>
+            <button type="button" className="btn btn-success">
+              Let's begin
+            </button>
+          </div>
+        </section>
+
+        <section className="d-flex row">
+          <div className="col-sm mt-5 section2-container">
+            <img src="./images/chef-hat.png" alt="fork" />
+            <h4 className="hello">Be the chef</h4>
+            <p className="text-muted">
+              Share your way of cook in a step-by-step recipe
+            </p>
+          </div>
+
+          <div className="col-sm mt-5 section2-container">
+            <img src="./images/cook.png" alt="fork" />
+            <h4 className="hello">Find the ideal recipe</h4>
+            <p className="text-muted">
+              Over 1,000 vegan, vegetarian and wolrdwide recipes for all levels
+            </p>
+          </div>
+
+          <div className="col-sm mt-5 section2-container">
+            <img src="./images/powder.png" alt="fork" />
+            <h4 className="hello">Fork and improve their recipes</h4>
+            <p className="text-muted">
+              As an open-source recipes base, GoGreen allow users suggesting
+              improvements to other chefs' recipe
+            </p>
+          </div>
         </section>
 
         <div
           className="container-fluid featured-recipes mt-5"
           style={{ width: "85%" }}
         >
+
+          <div className='my-5 d-flex flex-column align-items-center'>
+          <h3 className="">How skilled are you today?</h3>
+          <div className='div-bar'></div>
+          </div>
+
+
+
           <div className="row">
             <div className="col-sm">
               <a href="" className="recipe-link">
