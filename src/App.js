7f23528108286f99b12049c34828a4518bd6c42c
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-02-03 15:49:03 -0300

    Home and navbar changes

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ class App extends Component {
         })
         .catch(err => {
           this.setState({
-            loggedInUser:false
+            loggedInUser: false
           });
         });
     }
@@ -123,6 +123,7 @@ class App extends Component {
           <Route exact path='/' component={Home}/>
           <Route exact path='/login' render={(props) => <Login loggedInUser={this.state.loggedInUser} getUser={this.getUser} {...props} />} />
           <Route exact path='/signup' render={(props) => <Signup loggedInUser={this.state.loggedInUser} getUser={this.getUser} {...props} />}/>
+          {/* <Route exact path='/logout' render={{props}} => <Home loggoutUser={} /> */}
           <Route exact path='/aboutus' component={AboutUs}/>
           <Route exact path='/allrecipes' render={(props) => <AllRecipes allData={this.state} {...props} />} />
           <Route exact path='/addrecipe' render={(props) => <AddRecipe allData={this.state} {...props} /> } />
