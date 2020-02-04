be214455b98ab99b2aa616ed3ef4b7d6f7521493
Giu Roperto <giu.roperto@gmail.com>
2020-02-04 17:05:39 -0300

    adjusting delete

--- src/components/ConfirmDelete.js
+++ src/components/ConfirmDelete.js
@@ -1,24 +1,49 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import APIAccess from './api/api-access';
 
 class ConfirmDelete extends Component {
   constructor(props) {
     super(props);
+
+    this.apiEndpoints = new APIAccess();
+    this.deleteUser = this.deleteUser.bind(this);
+    this.redirectPage = this.redirectPage.bind(this);
+
+  }
+
+  deleteUser() {
+    const { username } = this.props.loggedInUser;
+    this.apiEndpoints.delete(username)
+      .then(response => {
+        this.props.getMessage(response.status, response.data.message);
+        this.redirectPage(this.props.successMessage, username)
+        // redirecionar e mostrar mensagem
+      })
+      .catch(err => console.log(err));
+  }
+
+  redirectPage(success, username) {
+    if (success) {
+      // redirecionar para a pagina
+      // this.props.history.push(`/user/${username}`);
+    }
   }
 
   render() {
     return (
       <div className="confirm-delete">
-        <Link to='/'>DELETE </Link>
+        <Link to='/' onClick={this.deleteUser}>DELETE </Link>
         <Link to={`/user/${this.props.loggedInUser.username}`}> CANCEL </Link>
       </div>
     )
   }
 
-
 }
 
 export default ConfirmDelete;
+
+// passar get message, successmessage, username, loggedinuser
 
 // passar username por props
 // when deleting, first logout and then delete user and redirect home
