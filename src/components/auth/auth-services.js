43615429301e0b88dce5d4e9f31e1c1eb4807720
Giu Roperto <giu.roperto@gmail.com>
2020-02-05 21:20:38 -0300

    deploy netlify 10

--- src/components/auth/auth-services.js
+++ src/components/auth/auth-services.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class AuthService {
   constructor() {
     this.service = axios.create({
-      baseURL: process.env.REACT_APP_API,
+      baseURL: 'http://go-green-recipes.herokuapp.com/api',
       withCredentials: true,
     });
   }
